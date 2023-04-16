c3cf20441a9e934e65af5ad775e2c657c0cb740c
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2023-04-16 20:21:30 +0530

    login function added, and completed register checks

--- server.js
+++ server.js
@@ -18,60 +18,107 @@ function Register(event){
     if(name && email && password && confirmPassword){
         if(password.length >= 8 && confirmPassword.length >= 8){
             if(password==confirmPassword){
-                var userData = {userName: name, userEmail:email, password: password, confirmPasssword: confirmPassword};
-            // console.log(userData);
-                localStorage.setItem("userx", JSON.stringify(userData));
-                // console.log(userData);
-                // console.log(JSON.stringify(userData));
+                var LS = JSON.parse(localStorage.getItem("users")) || [];
+                var flag=false;
+
+                for(var i=0; i< LS.length; i++){
+                    if(LS[i].userEmail == email){
+                        flag=true;
+                    }
+                }
+                if(!flag){
+                    var userData = {userName: name, userEmail: email, userPassword: password, userConfirmPassword: confirmPassword};
+                    LS.push(userData);
+                    localStorage.setItem("users" , JSON.stringify(LS));
+                    alert("Registration Successful!");
+                    document.getElementById("userName").value="";
+                    document.getElementById("userEmail").value="";
+                    document.getElementById("userPassword").value="";
+                    document.getElementById("userConfirmPassword").value="";
+                }
+                else{
+                    alert("Email Matches");
+                }
             } else {
                 console.log("Password do not Match");
             }
         } else {
             console.log("Enter 8 characters or more");
         }
-    } else{
+    } else {
         console.log("All Fields are Mandatory");
     }
 
 }
+
+
 
 function getData(){
-    console.log(JSON.parse(localStorage.getItem("userx")));
+    console.log(JSON.parse(localStorage.getItem("users")));
 }
 
 
-function MultipleUser(event){
+
+function Validate(event){
     event.preventDefault();
 
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
     
-    var name = document.getElementById("userName").value;
-    var email = document.getElementById("userEmail").value;
-    var password = document.getElementById("userPassword").value;
-    var confirmPassword = document.getElementById("userConfirmPassword").value;
 
-
-    if(name && email && password && confirmPassword){
-        if(password.length >= 8 && confirmPassword.length >= 8){
-            if(password==confirmPassword){
-                var userData = {userName: name, userEmail:email, password: password, confirmPasssword: confirmPassword};
-                // localStorage.setItem("userx", JSON.stringify(userData));
-                arrayData.push(userData);
-                // console.log(arrayData.length);
-                localStorage.setItem("userInfo", JSON.stringify(arrayData));
-                
-            } else {
-                console.log("Password do not Match");
-            }
-        } else {
-            console.log("Enter 8 characters or more");
+    var LS = JSON.parse(localStorage.getItem("users")) || [];
+    var flag=false;
+    for(var i=0;i<LS.length;i++){
+        if(LS[i].userEmail == email && LS[i].userPassword == password){
+            flag=true;
         }
-    } else{
-        console.log("All Fields are Mandatory");
+    }
+
+    if(flag){
+        alert("Login Successful");
+    } else {
+        alert("Credentials Failed");
     }
 
     
-
 }
+
+
+
+
+
+
+
+// function MultipleUser(event){
+//     event.preventDefault();
+
+    
+//     var name = document.getElementById("userName").value;
+//     var email = document.getElementById("userEmail").value;
+//     var password = document.getElementById("userPassword").value;
+//     var confirmPassword = document.getElementById("userConfirmPassword").value;
+
+
+//     if(name && email && password && confirmPassword){
+//         if(password.length >= 8 && confirmPassword.length >= 8){
+//             if(password==confirmPassword){
+//                 var userData = {userName: name, userEmail:email, password: password, confirmPasssword: confirmPassword};
+//                 arrayData.push(userData);
+//                 localStorage.setItem("userInfo", JSON.stringify(arrayData));
+                
+//             } else {
+//                 console.log("Password do not Match");
+//             }
+//         } else {
+//             console.log("Enter 8 characters or more");
+//         }
+//     } else{
+//         console.log("All Fields are Mandatory");
+//     }
+
+    
+
+// }
 
 
 
